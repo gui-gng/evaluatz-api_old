e55e4dac0587ea6cc50dff70fca856290f3d95f7
guigng <guigng@gmail.com>
2019-10-08 23:12:27 +1000

    Get Users

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var cors = require('cors');
 const indexRouter = require('./routes/index');
 const authRouter = require('./routes/auth');
 const signupRouter = require('./routes/signup');
+const userRouter = require('./routes/user');
 
 var app = express();
 
@@ -63,6 +64,7 @@ app.use(cors());
 app.use('/'       , indexRouter);
 app.use('/auth' , authRouter);
 app.use('/signup' , signupRouter);
+app.use('/user/:method/:value', userRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/user.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+const Users = require('../DAO/users');
+
+router.get('/', async function (req, res, next) {
+  res.set("Cached-Control", "public, max-age=300, s-maxage-600");
+    let method = req.params.method;
+    let value = req.params.value;
+  switch(method){
+      case 1:
+          res.send(getUserByUsername(value));
+      default:
+        res.send("Params not defined");
+  }
+  
+});
+
+function getUserByUsername(username){
+    return Users.getUserUsernameEmail(username);
+}
+
+module.exports = router;
