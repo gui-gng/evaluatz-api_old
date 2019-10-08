b8f3caebcfdedb84f3139b152a0c9c299ae2802a
guigng <guigng@gmail.com>
2019-10-09 06:08:39 +1000

    Fix CORS

--- routes/user.js
+++ routes/user.js
@@ -4,10 +4,10 @@ var router = express.Router();
 const Users = require('../DAO/users');
 
 router.get('/:method/:value', async function (req, res, next) {
-    res.set("Cached-Control", "public, max-age=300, s-maxage-600");
+    
     let method = req.params.method;
     let value = req.params.value;
-    // res.send(req.params);
+   
     switch (method) {
         case "1":
             res.send(await getUserByUsername(value));
