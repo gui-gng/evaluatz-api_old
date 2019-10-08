0181689a9d68f7d2d77109db8734cd6327aa39ea
guigng <guigng@gmail.com>
2019-10-08 23:26:35 +1000

    change to str

--- routes/user.js
+++ routes/user.js
@@ -4,19 +4,19 @@ var router = express.Router();
 const Users = require('../DAO/users');
 
 router.get('/', async function (req, res, next) {
-  res.set("Cached-Control", "public, max-age=300, s-maxage-600");
+    res.set("Cached-Control", "public, max-age=300, s-maxage-600");
     let method = req.params.method;
     let value = req.params.value;
-  switch(method){
-      case 1:
-          res.send(getUserByUsername(value));
-      default:
-        res.send("Params not defined");
-  }
-  
+    switch (method) {
+        case "1":
+            res.send(getUserByUsername(value));
+        default:
+            res.send("Params not defined");
+    }
+
 });
 
-function getUserByUsername(username){
+function getUserByUsername(username) {
     return Users.getUserUsernameEmail(username);
 }
 
