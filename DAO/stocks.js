10c964ad91123ec40adcbb9de0e9c975ca7f5030
guigng <guigng@gmail.com>
2019-10-27 15:39:12 +1000

    Updates Stock

--- DAO/stocks.js
+++ DAO/stocks.js
@@ -1,13 +1,22 @@
 const db = require('./database');
 let pool = db.createPool();
- 
+const axios = require('axios');
 
-module.exports =  {
-    getStock:  async (symbol, startDate, endDate) => {
-        return  await getStock(symbol, startDate, endDate);
+
+
+module.exports = {
+    getStock: async (source, symbol, startDate, endDate) => {
+        switch (source) {
+            case 'ASX':
+                return await getStockASX(symbol, startDate, endDate);
+            case 'NASDAQ':
+                return await getStockNASDAQ(symbol, startDate, endDate);
+            default:
+                return { Error: "Source unavailable" }
+        }
     },
     search: async (symbol) => {
-        return  await search(symbol);
+        return await search(symbol);
     },
     insertHistoric: async (symbol, date, open, high, low, close, volume) => {
         return await insertHistoric(symbol, date, open, high, low, close, volume);
@@ -18,7 +27,7 @@ module.exports =  {
 }
 
 
-async function getAllStocks(){
+async function getAllStocks() {
     return new Promise(async (resolve) => {
         let sqlQuery = "SELECT * FROM public.stocks";
         let values = [];
@@ -26,7 +35,7 @@ async function getAllStocks(){
     });
 }
 
-async function search(symbol){
+async function search(symbol) {
     return new Promise(async (resolve) => {
         console.log("Searching: " + symbol);
         let sqlQuery = "SELECT * FROM public.stocks WHERE company_name LIKE $1 OR symbol LIKE $1 LIMIT 10;";
@@ -35,7 +44,7 @@ async function search(symbol){
     });
 }
 
-async function getStock(symbol, startDate, endDate){
+async function getStockASX(symbol, startDate, endDate) {
     return new Promise(async (resolve) => {
         let sqlQuery = "SELECT company_name, symbol, industry FROM public.stocks WHERE symbol = $1;";
         let values = [symbol];
@@ -51,10 +60,39 @@ async function getStock(symbol, startDate, endDate){
     });
 }
 
-async function insertHistoric(symbol, date, open, high, low, close, volume){
+async function insertHistoric(symbol, date, open, high, low, close, volume) {
     return new Promise(async (resolve) => {
         let sqlQuery = "INSERT INTO public.stocks_historic(symbol, date, open, high, low, close, volume) VALUES ($1, $2, $3, $4, $5, $6, $7);";
         let values = [symbol, date, open, high, low, close, volume];
         resolve(await db.execute(pool, sqlQuery, values));
     });
+}
+
+
+async function getStockNASDAQ(symbol, startDate, endDate) {
+    let sqlQuery = "SELECT company_name, symbol, industry FROM public.stocks WHERE symbol = $1;";
+    let values = [symbol];
+    let response = await db.execute(pool, sqlQuery, values);
+    let stock = response[0];
+
+    const url = 'https://sandbox.tradier.com/v1/markets/history?' +
+        "symbol=" + symbol + "&" +
+        "interval=daily&" +
+        "start=" + startDate + "&" +
+        "end=" + endDate;
+
+    const options = {
+        url,
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Bearer eOkJXLeAAMXUAxUprOd96TXdZsJP',
+        }
+    };
+
+    axios(options).then(response => {
+        stock['historic'] = response.data.history.day;
+        resolve(stock);
+    })
+        .catch(error => console.error(error));
 }
--- routes/stocks.js
+++ routes/stocks.js
@@ -3,18 +3,18 @@ var router = express.Router();
 
 const StocksDAO = require('../DAO/stocks');
 
-router.get('/:id', async function (req, res, next) {
+router.get('/:source/:symbol', async function (req, res, next) {
     try {
         const end = new Date();
         const start = new Date(new Date().getTime() - ((end.getDay() % 6 + 6) * 24 * 60 * 60 * 1000));
-
         const startDate = req.query.startDate    || start.toISOString().split("T")[0];
         const finishDate = req.query.finishDate  || end.toISOString().split("T")[0];
-        const stocks = await StocksDAO.getStock(req.params.id, startDate, finishDate);
+        const stocks = await StocksDAO.getStock(req.params.source, req.params.symbol, startDate, finishDate);
         res.send(stocks);
     } catch (error) {
         res.send(error);
     }
 });
+
 
 module.exports = router;
