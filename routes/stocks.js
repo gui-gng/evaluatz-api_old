64b465901d665c251fdbf61fc23fc33192632ba2
guigng <guigng@gmail.com>
2019-10-27 20:30:30 +1000

    Update Stocks

--- routes/stocks.js
+++ routes/stocks.js
@@ -3,19 +3,6 @@ var router = express.Router();
 
 const StocksDAO = require('../DAO/stocks');
 
-router.get('/:source/:symbol', async function (req, res, next) {
-    try {
-        const end = new Date();
-        const start = new Date(new Date().getTime() - ((end.getDay() % 6 + 6) * 24 * 60 * 60 * 1000));
-        const startDate = req.query.startDate    || start.toISOString().split("T")[0];
-        const finishDate = req.query.finishDate  || end.toISOString().split("T")[0];
-        const stocks = await StocksDAO.getStock(req.params.source, req.params.symbol, startDate, finishDate);
-        res.send(stocks);
-    } catch (error) {
-        res.send(error);
-    }
-});
-
 
 router.get('/list', async function (req, res, next) {
     try {
@@ -26,10 +13,23 @@ router.get('/list', async function (req, res, next) {
     }
 });
 
-
 router.get('/search/:symbol', async function (req, res, next) {
     try {
         const stocks = await StocksDAO.search(req.params.symbol);
+        res.send(stocks);
+    } catch (error) {
+        res.send(error);
+    }
+});
+
+
+router.get('/:source/:symbol', async function (req, res, next) {
+    try {
+        const end = new Date();
+        const start = new Date(new Date().getTime() - ((end.getDay() % 6 + 6) * 24 * 60 * 60 * 1000));
+        const startDate = req.query.startDate    || start.toISOString().split("T")[0];
+        const finishDate = req.query.finishDate  || end.toISOString().split("T")[0];
+        const stocks = await StocksDAO.getStock(req.params.source, req.params.symbol, startDate, finishDate);
         res.send(stocks);
     } catch (error) {
         res.send(error);
